90644308bb995097af7b66af12ef28a5279677fc
Adam Schubert <adam.schubert@sg1-game.net>
2022-07-26 17:09:00 +0200

    feat(Loader): Added option to use alternative loader

--- xscreensaver_appearing_picture/bin/xscreensaver_appearing_picture.py
+++ xscreensaver_appearing_picture/bin/xscreensaver_appearing_picture.py
@@ -4,11 +4,12 @@ This is xscreensaver-appearing-picture.
 License: GPL-3.0
 Website: https://github.com/Salamek/xscreensaver-appearing-picture
 Usage:
-    xscreensaver-appearing-picture [--picture_path PICTURE_PATH] [--windowed] [--show_fps] [--animation_speed SPEED] [--fps FPS] [--background_color BACKGROUND_COLOR] [-window-id WINDOW_ID] [--display_time DISPLAY_TIME]
+    xscreensaver-appearing-picture [--picture_path PICTURE_PATH] [--windowed] [--show_fps] [--animation_speed SPEED] [--fps FPS] [--background_color BACKGROUND_COLOR] [-window-id WINDOW_ID] [--display_time DISPLAY_TIME] [--alternative_loader]
     xscreensaver-appearing-picture (-h | --help)
 Options:
     -window-id WINDOW_ID                              Screensaver ID of window to run in.
     --windowed                                        Run in window.
+    -a --alternative_loader                              Use alternative picture loader instead of xscreensaver integrated one.
     -f --show_fps                                     Show FPS.
     -c --picture_path PICTURE_PATH                    Screensaver path to picture to show.
     -t --display_time DISPLAY_TIME                    Display rendered image for number of seconds. [default: 5]
@@ -16,14 +17,16 @@ Options:
     -p --fps FPS                                      Screensaver FPS cap. [default: 30]
     -b --background_color BACKGROUND_COLOR            Screensaver background color. [default: #000000]
 """
-
+import datetime
 import os
 import signal
 import sys
 import subprocess
 import shutil
+import random
 
 from pathlib import Path
+from typing import List
 from functools import wraps
 from xscreensaver_config.ConfigParser import ConfigParser
 from xscreensaver_appearing_picture.Screensaver import Screensaver
@@ -76,40 +79,69 @@ def command(default: bool = False):
     return decorator
 
 
-def find_cat() -> str:
-    cat_files = [f for f in [
-        os.path.join('/', 'usr', 'share', 'xscreensaver-appearing-picture', 'cat.png'),
-        os.path.abspath(os.path.join(APP_ROOT_FOLDER, '..', 'usr', 'share', 'xscreensaver-appearing-picture', 'cat.png')),
-        os.path.join(APP_ROOT_FOLDER, '..', 'cat.png'),
-    ] if os.path.exists(f)]
+class ImageResolver:
+    _image_list = []
+    _image_list_creation_time = None
 
-    if len(cat_files) == 0:
-        raise ValueError('cat.png was not found in any search path')
+    def _find_cat(self) -> str:
+        cat_files = [f for f in [
+            os.path.join('/', 'usr', 'share', 'xscreensaver-appearing-picture', 'cat.png'),
+            os.path.abspath(os.path.join(APP_ROOT_FOLDER, '..', 'usr', 'share', 'xscreensaver-appearing-picture', 'cat.png')),
+            os.path.join(APP_ROOT_FOLDER, '..', 'cat.png'),
+        ] if os.path.exists(f)]
 
-    return cat_files[0]
+        if len(cat_files) == 0:
+            raise ValueError('cat.png was not found in any search path')
+
+        return cat_files[0]
+
+    def _get_image_list(self, path: Path) -> List[Path]:
+        if self._image_list_creation_time and self._image_list_creation_time + datetime.timedelta(seconds=60) < datetime.datetime.now():
+            # Reset _image_list to force reload
+            self._image_list = []
+
+        if not self._image_list:
+            for file in path.glob('*'):
+                if file.is_file() and file.suffix.strip('.') in ['gif', 'jpeg', 'jpg', 'png', 'bmp']:
+                    self._image_list.append(file)
+            self._image_list_creation_time = datetime.datetime.now()
+
+        return self._image_list
+
+    def get_default_image(self) -> str:
+        try:
+            config_parser = ConfigParser(os.path.join(os.path.expanduser('~'), '.xscreensaver'))
+            config_parsed = config_parser.read()
+
+            image_directory_path = Path(config_parsed.get('imageDirectory'))
+            if OPTIONS['--alternative_loader']:
+                image_list = self._get_image_list(image_directory_path)
+                if not image_list:
+                    raise FileNotFoundError
+                random_image_path = random.choice(image_list)
+                if not random_image_path.is_file():
+                    raise FileNotFoundError
+                return str(random_image_path)
+
+            found_xscreensaver_image_path = shutil.which('xscreensaver-getimage-file')
+            if found_xscreensaver_image_path:
+                random_image_name = subprocess.check_output(found_xscreensaver_image_path).decode('UTF-8').strip()
+                random_image_path = image_directory_path.joinpath(random_image_name)
+                if not random_image_path.is_file():
+                    raise FileNotFoundError
+                return str(random_image_path)
+        except (FileNotFoundError, subprocess.CalledProcessError):
+            pass
+
+        return self._find_cat()
 
 
 @command(default=True)
 def run():
-
-    def _get_default_image() -> str:
-        try:
-            config_parser = ConfigParser(os.path.join(os.path.expanduser('~'), '.xscreensaver'))
-            config_parsed = config_parser.read()
-            found_xscreensaver_image_path = shutil.which('xscreensaver-getimage-file')
-            if found_xscreensaver_image_path:
-                random_image_name = subprocess.check_output(found_xscreensaver_image_path).decode('UTF-8').strip()
-                random_image_path = os.path.join(config_parsed.get('imageDirectory'), random_image_name)
-                if not os.path.isfile(random_image_path):
-                    raise FileNotFoundError
-                return random_image_path
-        except (FileNotFoundError, subprocess.CalledProcessError):
-            pass
-
-        return find_cat()
+    image_resolver = ImageResolver()
 
     def picture_path_callable() -> Path:
-        picture_path = Path(OPTIONS['--picture_path'] if OPTIONS['--picture_path'] else _get_default_image())
+        picture_path = Path(OPTIONS['--picture_path'] if OPTIONS['--picture_path'] else image_resolver.get_default_image())
         if not picture_path.is_file():
             raise ValueError('Path {} is not valid file.'.format(picture_path.absolute()))
 
